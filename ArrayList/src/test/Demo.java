72a3a9c0cd9672c2f8033d550c6b7b2586ece29e
B Hemant Kumar <hemant.ku101@gmail.com>
2023-02-23 20:21:06 +0530

    Demo ArrayList add()

---
+++ ArrayList/src/test/Demo.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.ArrayList;
+
+public class Demo {
+	@SuppressWarnings({"rawtypes","unchecked"})
+	public static void main(String args)
+	{
+		ArrayList al=new ArrayList();
+		al.add(45);
+		al.add(83);
+		al.add(120);
+		al.add(15);
+		al.add(35);
+		System.out.println(al);
+	}
+}
