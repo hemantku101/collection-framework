20ed9676d31c8e4953ae8469f5c8ff48b853bfe4
B Hemant Kumar <hemant.ku101@gmail.com>
2023-02-24 12:59:55 +0530

    LinkedHashSet

---
+++ LinkedHashSet/src/test/Demo.java
@@ -0,0 +1,14 @@
+package test;
+import java.util.LinkedHashSet;
+public class Demo {
+	public static void main(String args[])
+	{
+		LinkedHashSet<Integer> lhs=new LinkedHashSet<>();
+		lhs.add(56);
+		lhs.add(22);
+		lhs.add(58);
+		lhs.add(74);
+		lhs.add(10);
+		System.out.println(lhs);
+	}
+}
