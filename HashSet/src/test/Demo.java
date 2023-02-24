23eee00e603e3a750954c1536100c59bdc3c18c4
B Hemant Kumar <hemant.ku101@gmail.com>
2023-02-24 12:06:18 +0530

    HashSet

---
+++ HashSet/src/test/Demo.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.util.HashSet;
+public class Demo {
+	public static void main(String[] args) {
+		HashSet<Integer> hs=new HashSet<>();
+		hs.add(20);
+		hs.add(46);
+		hs.add(33);
+		hs.add(16);
+		hs.add(98);
+		
+		System.out.println(hs);
+	}
+}
